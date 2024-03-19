af9c5101357745ee688e6887e0f1401997944d80
unknown <m2m.mostafa@gmail.com>
2024-03-19 21:36:37 +0330

    init project

---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Vazirmatn, Inter , Lusitana } from 'next/font/google';
+export const inter = Inter({ subsets: ['latin'] });
+export const vazirmatn = Vazirmatn({subsets : ['arabic']});
+export const lusitana = Lusitana({weight: ['400' , '700'], subsets: ['latin']});
